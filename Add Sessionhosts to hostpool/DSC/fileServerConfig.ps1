af291e3b26149edf3d767f04108a7feb24b7af2b
People Tech (CSE) <admin-cse@peopletech.com>
2020-07-19 13:23:09 +0530

    Added Ubikite3-0 files

---
+++ Add Sessionhosts to hostpool/DSC/fileServerConfig.ps1
@@ -0,0 +1,28 @@
+﻿param(
+[Parameter(Mandatory = $true)]
+[ValidateNotNullOrEmpty()]
+[string] $HostpoolFolderName,
+[Parameter(Mandatory = $true)]
+[ValidateNotNullOrEmpty()]
+[string] $Usernames
+)
+
+$RAWDisks = Get-Disk | Where-Object PartitionStyle –Eq 'RAW'
+
+foreach($Disk in $RAWDisks){
+# Initializing disk with MBR
+Initialize-Disk -Number $Disk.number -PartitionStyle MBR
+New-Partition –DiskNumber $Disk.number -AssignDriveLetter –UseMaximumSize
+$DiskPartition = Get-Partition -DiskNumber $Disk.number
+Set-Partition -DriveLetter $DiskPartition.DriveLetter -IsActive $true
+Format-Volume -DriveLetter $DiskPartition.DriveLetter -FileSystem NTFS
+$DiskLetter = $DiskPartition.DriveLetter
+$DiskDrive = $DiskLetter + ":\"
+New-Item -Name $HostpoolFolderName -Path $DiskDrive -ItemType Directory
+$Path = $DiskDrive + $HostpoolFolderName
+$c = new-CimSession -ComputerName $env:COMPUTERNAME
+New-SmbShare -Name $HostpoolFolderName -Path $Path -CimSession $c `
+  -Description 'Shared Folder for $HostpoolName users' `
+  -FullAccess $Usernames -ReadAccess Everyone -ChangeAccess Everyone
+  }
+
