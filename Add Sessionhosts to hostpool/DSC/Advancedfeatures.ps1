57b57937c7cb04b0f72c1a8904ae9f9afdbb8752
People Tech (CSE) <admin-cse@peopletech.com>
2021-02-12 15:32:50 +0530

    aligned advancedfeature script file with some changes

--- Add Sessionhosts to hostpool/DSC/Advancedfeatures.ps1
+++ Add Sessionhosts to hostpool/DSC/Advancedfeatures.ps1
@@ -69,22 +69,6 @@ cmd /c "icacls b: /grant `${`$SignInName}:(f)"
     }
 }
 
-if ($IsTeamsAppSetup -eq "true") {
-    # Add registery of microsoft teams inside sessionhost
-    REG add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
-    $TeamsInstaller = "https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.4461/Teams_windows_x64.msi"
-    $WebRTC = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4vkL6"
-    $MicrosoftViualCpp = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
-    $DestinationPath = "C:\TeamsInstallers"
-    New-Item -Path $DestinationPath -ItemType "directory"
-    Invoke-WebRequest -Uri $TeamsInstaller -OutFile $DestinationPath\Teams_windows_x64-01.msi
-    $TeamAppInstallerPath = Get-ChildItem -LiteralPath $DestinationPath | Where-Object {$_.Name -match "Teams_windows*"} | select FullName
-    msiexec /i $TeamAppInstallerPath.FullName /l*v c:\TeamsInstallers\Teams.log ALLUSER=1 ALLUSERS=1
-    Invoke-WebRequest -Uri $MicrosoftViualCpp -OutFile $DestinationPath\vc_redist.x64.exe
-    Invoke-WebRequest -Uri $WebRTC -OutFile $DestinationPath\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi
-    Invoke-Expression -Command "cmd.exe /c 'c:\TeamsInstallers\vc_redist.x64.exe' /quiet"
-    Invoke-Expression -Command "cmd.exe /c 'c:\TeamsInstallers\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi' /quiet"
-}
 
 if ($IsUserSessionShadow -eq "true") {
     $GpDownloadURI = "https://raw.githubusercontent.com/people-tech-group/ubikite/master/Add%20Sessionhosts%20to%20hostpool/DSC/GroupPolicies.zip"
@@ -138,4 +122,21 @@ if ($IsUserSessionShadow -eq "true") {
             gpupdate /force
         } 
     }
+}
+
+if ($IsTeamsAppSetup -eq "true") {
+    # Add registery of microsoft teams inside sessionhost
+    REG add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
+    $TeamsInstaller = "https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.4461/Teams_windows_x64.msi"
+    $WebRTC = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4vkL6"
+    $MicrosoftViualCpp = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
+    $DestinationPath = "C:\TeamsInstallers"
+    New-Item -Path $DestinationPath -ItemType "directory"
+    Invoke-WebRequest -Uri $TeamsInstaller -OutFile $DestinationPath\Teams_windows_x64-01.msi
+    $TeamAppInstallerPath = Get-ChildItem -LiteralPath $DestinationPath | Where-Object {$_.Name -match "Teams_windows*"} | select FullName
+    msiexec /i $TeamAppInstallerPath.FullName /l*v c:\TeamsInstallers\Teams.log ALLUSER=1 ALLUSERS=1
+    Invoke-WebRequest -Uri $MicrosoftViualCpp -OutFile $DestinationPath\vc_redist.x64.exe
+    Invoke-WebRequest -Uri $WebRTC -OutFile $DestinationPath\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi
+    Invoke-Expression -Command "cmd.exe /c 'c:\TeamsInstallers\vc_redist.x64.exe' /quiet"
+    Invoke-Expression -Command "cmd.exe /c 'c:\TeamsInstallers\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi' /quiet"
 }
